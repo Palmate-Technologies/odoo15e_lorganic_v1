882523d95698c63316660a54403bc8c93c0e293f
WaseemShaikh <wassim.sheikh@gmail.com>
2022-05-28 23:35:33 +0530

    pos receipt print arabic name for product

--- addons-pos/pos_receipt_arabic/__manifest__.py
+++ addons-pos/pos_receipt_arabic/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'POS Arabic Receipt',
-    'version': '15.0.0.4',
+    'version': '15.0.0.5',
     'sequence': 1,
     'summary': 'Pierce PPA',
     'description': """
--- addons-pos/pos_receipt_arabic/static/src/js/models.js
+++ addons-pos/pos_receipt_arabic/static/src/js/models.js
@@ -2,18 +2,16 @@ odoo.define('pos_receipt_arabic.models', function (require) {
 "use strict";
 
 var models = require('point_of_sale.models');
+models.load_fields('product.product', ['name_arabic']);
+var _super_orderline = models.Orderline.prototype;
 
-    var models = require('point_of_sale.models');
-    models.load_fields('product.product', ['name_arabic']);
-    var _super_Orderline = models.Orderline.prototype;
-
-    models.Orderline = models.Orderline.extend({
-        export_for_printing: function(){
-            var result = _super_Orderline.export_for_printing.apply(this,arguments);
-            result.product_name = this.get_product().display_name;
-            result.product_arabic_name = this.get_product().name_arabic;
-            return result;
-        },
-    });
+models.Orderline = models.Orderline.extend({
+    export_for_printing: function(){
+        var result = _super_orderline.export_for_printing.apply(this,arguments);
+        result.product_name = this.get_product().display_name;
+        result.product_arabic_name = this.get_product().name_arabic;
+        return result;
+    },
+});
 
 });
