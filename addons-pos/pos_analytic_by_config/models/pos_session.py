90c85edd66ca17794578cadb2a570af2ece318f7
WaseemShaikh <wassim.sheikh@gmail.com>
2023-07-13 20:22:37 +0530

    pos session closing issue fix: _get_sale_vals() parameter changed by odoo

--- addons-pos/pos_analytic_by_config/models/pos_session.py
+++ addons-pos/pos_analytic_by_config/models/pos_session.py
@@ -25,12 +25,12 @@ class PosSession(models.Model):
             partial_move_line_vals, amount, amount_converted, force_company_currency
         )
 
-    def _get_sale_vals(self, key, amount, amount_converted):
+    def _get_sale_vals(self, key, amount, amount_converted, tax_amount):
         """The method that allowed to add the analytic account to the sales items
         has been dropped in v13, so we have to add it in the moment the sales
         items values are prepared.
         """
         account_analytic_id = self.config_id.account_analytic_id
         if account_analytic_id:
-            return super(PosSession,self.with_context(account_analytic_id=account_analytic_id.id))._get_sale_vals(key, amount, amount_converted)
-        return super()._get_sale_vals(key, amount, amount_converted)
+            return super(PosSession,self.with_context(account_analytic_id=account_analytic_id.id))._get_sale_vals(key, amount, amount_converted, tax_amount)
+        return super()._get_sale_vals(key, amount, amount_converted, tax_amount)
